1323c990d757d40e4a4fcf18eb9b27fe96602383
nourka00 <amernourgo@gmail.com>
2025-03-05 11:22:14 +0200

    our frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Incomes from "./pages/Incomes";
+import Expenses from "./pages/Expenses";
+import Goals from "./pages/profitGoals";
+import Login from "./pages/login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Incomes />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/profit-goals" element={<Goals />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
