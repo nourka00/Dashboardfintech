ec11a7f000b79d3b7a7d7c832bfdf812efeae31d
nourka00 <amernourgo@gmail.com>
2025-03-06 17:00:06 +0200

    Updated Navbar and Home pages for routing improvements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Incomes from "./pages/Incomes";
 import Expenses from "./pages/Expenses";
 import Goals from "./pages/profitGoals";
-import Login from "./pages/login";
+import Admin from "./pages/Admin";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   
-      <Login />
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <div style={{ marginLeft: "392px", padding: "20px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/incomes" element={<Incomes />} />
+            <Route path="/expenses" element={<Expenses />} />
+            <Route path="/goals" element={<Goals />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import userImage from "../assets/userprofile.png";
+import "../CSS/navbar.css";
+import { NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h3>
+        Finance Tracker <br /> System
+      </h3>
+      <div className="sidebar">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/incomes"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Incomes
+        </NavLink>
+        <NavLink
+          to="/expenses"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Expenses
+        </NavLink>
+        <NavLink
+          to="/goals"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Profit Goals
+        </NavLink>
+        <NavLink
+          to="/admin"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Admin Page
+        </NavLink>
+      </div>
+
+      <div className="user-profile">
+        <img src={userImage} alt="User Profile" />
+        <p>User</p>
+        <p>user@email.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
